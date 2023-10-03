52a37e614e036c168fd5acbd6b179272678412bc
Alex5 <Alex5@TOLEDO521>
2023-10-02 18:30:22 -0600

    Agregar archivos de proyecto.

---
+++ Enteros de Arreglos 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50, 60 };
+
+        int numeroDeseado = numeros[1];
+
+        Console.WriteLine("el numero deseado es: " + numeroDeseado);
+
+        Console.ReadLine();
+    }
+}
